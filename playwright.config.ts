e933606f932fcf51efa2760b44e0c5f2fb688ab9
Felipediet <fefola@gmail.com>
2026-05-20 16:57:54 -0300

    Update environment configuration and tests for deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:5173/',
+    //    baseURL: 'http://localhost:5173/',
+    baseURL: process.env.BASE_URL || 'https://velodiet.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // trace: 'retain-on-failure',
